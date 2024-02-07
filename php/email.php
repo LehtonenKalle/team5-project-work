6c6864a62041d1ac805f90245408577d7293be11
LauriSantala <lauri23008@student.hamk.fi>
2024-02-07 09:55:45 +0200

    fixed email.php file

--- php/email.php
+++ php/email.php
@@ -4,17 +4,17 @@ include("../connect.php");
 // katsotaan onko lähetetty tietoa
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // otetaan data vastaan
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
     $email = $_POST['email'];
     $comment = $_POST['comment'];
 
     // Syötetään tiedot tietokantaan
-    $sql = "INSERT INTO palaute (first_name, last_name, email, comment) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO palaute (firstname, lastname, email, comment) VALUES (?, ?, ?, ?)";
     
     // Bind parameters and execute the statement
     $stmt = $yhteys->prepare($sql);
-    $stmt->bind_param("ssss", $first_name, $last_name, $email, $comment);
+    $stmt->bind_param("ssss", $firstname, $lastname, $email, $comment);
     $stmt->execute();
     
     // Close statement
