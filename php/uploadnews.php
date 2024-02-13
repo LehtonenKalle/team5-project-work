4e37eeef3d99febd67804d3c1a37ce93fe1ff521
LauriSantala <lauri23008@student.hamk.fi>
2024-02-13 12:34:13 +0200

    Changed uploadnews so it will upload picture to folder and then to database

--- php/uploadnews.php
+++ php/uploadnews.php
@@ -1,58 +1,60 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Käsitellään kuvin lataus
-    $image_url = null;
-    $uploadOk = 1;
+    if(isset($_POST["title"]) && isset($_POST["content"]) && isset($_FILES["image"])) {
+        // Yhteys Tietokantaan
+        include("connect.php");
 
-    // tarkistetaan on kyseessä kuva vai ei
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
-        // tiedosto on kuva
+        // Tiedon tallennus
+        $target_dir = "images/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
         $uploadOk = 1;
-    } else {
-        print "File is not an image.";
-        $uploadOk = 0;
-    }
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // tarkistetaan tiedoston koko
-    if ($_FILES["image"]["size"] > 500000) {
-        print "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-
-    // Sallitaan tietyt tiedosto tyypit
-    $imageFileType = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-        print "Vain JPG, JPEG, PNG & GIF tiedostos sallittuja.";
-        $uploadOk = 0;
-    }
-
-    // Jatketaan jos kaikki on ok
-    if ($uploadOk == 1) {
-        $image_url = 'images/' . uniqid() . '_' . $_FILES["image"]["name"];
-            // Laitetaan tiedosto tietokantaan
-            if (move_uploaded_file($_FILES["image"]["tmp_name"], $image_url)){
-            
-            $title = $_POST["title"];
-            $content = $_POST["content"];
-
-            include("connect.php");
-
-            $sql = "INSERT INTO news (title, content, image_url) VALUES ('$title', '$content', '$image_url')";
-            if ($yhteys->query($sql) === TRUE) {
-                print "Tietojen lähetys onnistui";
-            } else {
-                print "Virhe: " . $sql . "<br>" . $yhteys->error;
-            }
-
-            $yhteys->close();
-        } else {
-            print "Tiedostoa ladattaessa tuli virhe.";
+        // Tarkistetaan onko kyseessä kuva vai ei
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check === false) {
+            print "Tiedosto ei ole kuva.";
+            $uploadOk = 0;
         }
-    } else {
-        print "Valitettavasti tiedostoasi ei ladattu.";
-    }
 
+        // tarkistetaan tiedoston koko
+        if ($_FILES["image"]["size"] > 500000) {
+            print "Tiedosto on liian suuri.";
+            $uploadOk = 0;
+        }
+
+        // Sallitaan vain tietyn tyyppiset tiedostot
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+            print "Vain JPG, JPEG, PNG & GIF tiedostot sallittu.";
+            $uploadOk = 0;
+        }
+
+        // Katsotaan onko virhettä
+        if ($uploadOk == 0) {
+            print "Virhe tiedostoa ei ladattu.";
+        } else {
+            // Yritetään ladata tiedot
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                // lataus onnistuu ja laitetaan tiedot tietokantaan
+                $title = $_POST["title"];
+                $content = $_POST["content"];
+                $image_url = $target_file;
+
+                $sql = "INSERT INTO news (title, content, image_url) VALUES ('$title', '$content', '$image_url')";
+                if ($yhteys->query($sql) === TRUE) {
+                    print "Onnistunut tiedon lataus.";
+                } else {
+                    print "Error: " . $sql . "<br>" . $yhteys->error;
+                }
+            } else {
+                print "Virhe tietoa ladatessa.";
+            }
+        }
+
+        // suljetaan yhteys
+        $yhteys->close();
+    } else {
+        print "Täytä kaikki kentät.";
+    }
 }
 ?>
