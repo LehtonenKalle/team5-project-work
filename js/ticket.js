ae3436544a6c154acbacd21b2fd3b017dd797a58
MiikaKonttila <miika20113@student.hamk.fi>
2024-02-14 10:58:08 +0200

    added comments
    explained the javascript and css in the code

--- js/ticket.js
+++ js/ticket.js
@@ -1,4 +1,4 @@
 function toggleDropdown(dropdownId) {
     var dropdown = document.getElementById(dropdownId);
-    dropdown.classList.toggle("show");
+    dropdown.classList.toggle("show"); /* Lisää tai poistaa "show" -luokan pudotusvalikosta */
 }
