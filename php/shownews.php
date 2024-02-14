b52b02e658710d06a3e98689b8c4a573885deb09
LehtonenKalle <kalle.lehtonen01@gmail.com>
2024-02-14 12:01:04 +0200

    update

--- php/shownews.php
+++ php/shownews.php
@@ -1,28 +1,25 @@
 <?php
+// Hakee databasesta tiedon
+$sql = "SELECT * FROM news ORDER BY id DESC";
+$result = $yhteys->query($sql);
 
-include("connect.php");
+print "<p>"."moro"."</p>";
 
-// // Hakee databasesta tiedon
-// $sql = "SELECT * FROM news ORDER BY id DESC";
-// $result = $yhteys->query($sql);
+// näyttää postauksen
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        print "<h2>" . $row["title"] . "</h2>";
+        print "<p>" . $row["content"] . "</p>";
+        if ($row["image_url"]) {
+            $imageData = base64_encode($row["image_data"]);
+            $src = 'data:image/' . $row["image_type"] . 'base64,' . $image_data;
+            print "<img src='" . $src . "' alt='Post Image'>";
+        }
+        print "<hr>";
+    }
+} else {
+    print "Ei uutisia.";
+}
 
-// print "<p>"."moro"."</p>";
-
-// // näyttää postauksen
-// if ($result->num_rows > 0) {
-//     while($row = $result->fetch_assoc()) {
-//         print "<h2>" . $row["title"] . "</h2>";
-//         print "<p>" . $row["content"] . "</p>";
-//         if ($row["image_url"]) {
-//             $imageData = base64_encode($row["image_data"]);
-//             $src = 'data:image/' . $row["image_type"] . 'base64,' . $image_data;
-//             print "<img src='" . $src . "' alt='Post Image'>";
-//         }
-//         print "<hr>";
-//     }
-// } else {
-//     print "Ei uutisia.";
-// }
-
-// $yhteys->close();
+$yhteys->close();
 ?>
